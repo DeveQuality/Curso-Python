f9f96de97e53614f598ae0a6156192e5a64e7cd3
Flora <fmacuacua116@gmail.com>
2025-12-21 22:21:27 -0800

    actualizei

--- mundo2/Exercicios/ex059.py
+++ mundo2/Exercicios/ex059.py
@@ -1,33 +1,38 @@
 n1=int(input('Primeiro numero: '))
 n2=int(input('Segundo numero: '))
 
-start=1
-while start>0:
+escolha=1
+while escolha!=5:
     print('-------------------------------------Resultado--------------------------------------')
     escolha=int(input(' [1]Somar \n [2]Multiplicar \n [3]Maior \n [4]Novos numeros \n [5]Sair do programa \nEscolha: '))
     print('-------------------------------------Resultado--------------------------------------')
-    if start!=5:
-        if escolha==1:
-            soma=n1+n2
-            print('A soma sera de {}'.format(soma))
-        if escolha==2:
-            multiplicar=n1*n2
-            print('A multiplicacao dos dois valores sera de {}'.format(multiplicar))
-        if escolha==3:
-            maior=max(n1,n2)
-            print('O maior valor sera {}'.format(maior))
+    if escolha==1:
+        soma=n1+n2
+        print('A soma sera de {}'.format(soma))
+    if escolha==2:
+        multiplicar=n1*n2
+        print('A multiplicacao dos dois valores sera de {}'.format(multiplicar))
+    if escolha==3:
+        maior=max(n1,n2)
+        print('O maior valor sera {}'.format(maior))
 
 
-        if escolha==4:
-            Nv_1=int(input('digite um novo numero: '))
-            Nv_2=int(input('digite um novo numero: '))
+    if escolha==4:
+        Nv_1=int(input('digite um primeiro novo numero: '))
+        Nv_2=int(input('digite um segundo novo numero: '))
+        
+        print('-------------------------------------Resultado_2--------------------------------------')
+        escolha_2=int(input(' [1]Somar \n [2]Multiplicar \n [3]Maior \n [4]Novos numeros \n [5]Sair do programa \nEscolha: '))
+        print('-------------------------------------Resultado_2--------------------------------------')
 
-            if escolha==1:
-                soma2=Nv_1+Nv_2
-                print('A soma sera de {}'.format(soma2))
-            if escolha==2:
-                multiplicar2=Nv_1*Nv_2
-                print('A multiplicacao dos dois valores sera de {}'.format(multiplicar2))
-            if escolha==3:
-                maior2=max(Nv_1,Nv_2)
-                print('O maior valor sera {}'.format(maior2))
+        if escolha_2==1:
+            soma2=Nv_1+Nv_2
+            print('A soma sera de {}'.format(soma2))
+        if escolha_2==2:
+            multiplicar2=Nv_1*Nv_2
+            print('A multiplicacao dos dois valores sera de {}'.format(multiplicar2))
+        if escolha_2==3:
+            maior2=max(Nv_1,Nv_2)
+            print('O maior valor sera {}'.format(maior2))
+
+print('Fim do programa...')
