7fc290d7cb6c3f9014a83fa5125ff157fb6462db
Flora <fmacuacua116@gmail.com>
2026-02-01 22:16:14 -0800

    actualizei

--- mundo2/Exercicios/ex057.py
+++ mundo2/Exercicios/ex057.py
@@ -1,7 +1,7 @@
-sexo=str(input('Type your gender(m/f): '))
+sexo=str(input('Type your gender(m/f): ')).strip()
 
 while sexo !='m' and sexo != 'f':
-    sexo=str(input('ERROR! Try again: '))
+    sexo=str(input('ERROR! Try again: ')).strip()
 if sexo=='f':
     print('You are woman')
 elif sexo=='m':
--- mundo2/Exercicios/ex058.py
+++ mundo2/Exercicios/ex058.py
@@ -2,12 +2,11 @@ import random
 comp=random.randint(0,10)
 tentantiva=0
 
-usuario=int(input('Tente adivinhar o numero que o computador escolheu: '))
+usuario=int(input('Tente adivinhar o numero que o computador escolheu entre 0 a 10: '))
 
 while comp != usuario:
    
     usuario=int(input('Voce perdeu ! Tente de novo: '))
     tentantiva+=1
 
-
-print('Voce acertou  mas precisou de {} tentativas'.format(tentantiva))
+print('Voce acertou  mas precisou de {} tentativa/s'.format(tentantiva))
