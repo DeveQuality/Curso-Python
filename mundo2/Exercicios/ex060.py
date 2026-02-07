a4bbb9ba65ca153178283c3b996fd768e43b31a0
Flora <fmacuacua116@gmail.com>
2026-02-06 22:10:44 -0800

    actualizei

--- mundo2/Exercicios/ex060.py
+++ mundo2/Exercicios/ex060.py
@@ -1,8 +1,26 @@
-num=int(input('Digite um numero: '))
+""" num=int(input('Digite um numero: '))
 
 start=1
 while num > 1:
     start=start*num
     num=num-1
 
-print(start)
+print(start) """
+
+""" from math import factorial
+
+numero=int(input('Digite um numero para obter o fatorial: '))
+f=factorial(numero)
+print(' O fatorial do numero {} sera {}'.format(numero,f)) """
+
+numero=int(input('Digite um numero para obter o fatorial: '))
+c=numero
+f=1
+print('Calculando {}! = ' .format(numero),end='')
+while c>0:
+    print('{}'.format(c), end='')
+    print(' x ' if c>1 else ' = ' , end='')
+
+    f*=c
+    c-=1
+print('{}'.format(f))
